40900121207472deaf8641464d544258177463f1
harihaysrun <nursyahirah98@outlook.com>
2022-03-08 12:53:42 +0000

    add main routes

--- routes/landing.js
+++ routes/landing.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router(); 
 
 router.get('/', function(req,res) {
-    res.send("Welcome")
+    res.render('landing/index')
 })
+
+router.get('/products', function(req,res) {
+    res.render('products/index')
+})
+
 
 module.exports = router;
