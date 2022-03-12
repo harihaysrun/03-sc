de0aec3f8704aee7face78014b65c4cc5c4c6895
harihaysrun <nursyahirah98@outlook.com>
2022-03-12 06:12:55 +0000

    add create function

---
+++ forms/index.js
@@ -0,0 +1,51 @@
+const forms = require("forms");
+
+const fields = forms.fields;
+const validators = forms.validators;
+const widgets = forms.widgets;
+
+const bootstrapField = function (name, object) {
+    if (!Array.isArray(object.widget.classes)) { object.widget.classes = []; }
+
+    if (object.widget.classes.indexOf('form-control') === -1) {
+        object.widget.classes.push('form-control');
+    }
+
+    var validationclass = object.value && !object.error ? 'is-valid' : '';
+    validationclass = object.error ? 'is-invalid' : validationclass;
+    if (validationclass) {
+        object.widget.classes.push(validationclass);
+    }
+
+    var label = object.labelHTML(name);
+    var error = object.error ? '<div class="invalid-feedback">' + object.error + '</div>' : '';
+
+    var widget = object.widget.toHTML(name, object);
+    return '<div class="form-group">' + label + widget + error + '</div>';
+};
+
+const createProductForm = function(categories, tags){
+    return forms.create({
+        // create text input called "name" etc.
+        "name":fields.string({
+            'required':true,
+            'errorAfterField':true
+        }),
+        "cost":fields.string({
+            'required':true,
+            'errorsAfterField':true,
+            'validators': [validators.integer(), validators.min(0)]
+        }),
+        "description":fields.string({
+            'required':true,
+            'errorAfterField':true
+        }),
+        "ingredients":fields.string({
+            'required':true,
+            'errorAfterField':true
+        }),
+    })
+}
+
+
+module.exports = {bootstrapField, createProductForm };
--- routes/products.js
+++ routes/products.js
@@ -2,11 +2,41 @@ const express = require("express");
 const router = express.Router();
 
 const { Product } = require('../models');
+const {bootstrapField, createProductForm } = require('../forms');
 
 router.get('/', async function(req,res){
     let products = await Product.collection().fetch();
     res.render('products/index',{
         'products': products.toJSON()
+    })
+});
+
+router.get('/create', async function(req,res){
+    const productForm = createProductForm();
+    res.render('products/create',{
+        'form': productForm.toHTML(bootstrapField)
+    })
+});
+
+router.post('/create', async function(req,res){
+    const productForm = createProductForm();
+    
+    productForm.handle(req,{
+        'success':async function(form){
+            const newProduct = new Product();
+            newProduct.set('name', form.data.name);
+            newProduct.set('cost', form.data.cost);
+            newProduct.set('description', form.data.description);
+            newProduct.set('ingredients', form.data.ingredients);
+            await newProduct.save();
+
+            res.redirect('/products');
+        },
+        'error':function(form){
+            res.render('products/create',{
+                'form':form.toHTML(bootstrapField)
+            })
+        }
     })
 });
 
