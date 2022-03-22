23adc5d9680025652c384e0c54d45b34fa7810bb
harihaysrun <nursyahirah98@outlook.com>
2022-03-22 12:55:36 +0000

    remove jwt auth

--- routes/api/cart.js
+++ routes/api/cart.js
@@ -16,10 +16,10 @@ router.get('/', async function(req,res){
     })
 })
 
-router.post('/:product_id/add', checkIfAuthenticatedWithJWT, async function(req,res){
+router.post('/:product_id/add', async function(req,res){
 
-    let userId = req.session.user.id;
-    // let userId = req.body.user_id;
+    // let userId = req.session.user.id;
+    let userId = req.body.user_id;
     let productId = req.params.product_id;
     let quantity = 1;
         
