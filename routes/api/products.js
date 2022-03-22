3dce1ed6f32685e636219b64658fd1595458e51b
harihaysrun <nursyahirah98@outlook.com>
2022-03-22 09:58:57 +0000

    retrieve product info using api

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,9 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 const productDataLayer = require('../../dal/products');
+const { Product } = require('../../models');
 
 router.get('/', async function(req,res){
     res.send(await productDataLayer.getAllProducts())
+})
+
+router.get('/products/:product_id', async function(req,res){
+
+    let productId = req.params.product_id;
+        
+    const product = await Product.where({
+        'id': productId
+    }).fetch({
+        require:true,
+        withRelated:['country', 'type', 'skinTypes', 'status']
+    })
+
+    res.json({product})
 })
 
 module.exports = router;
