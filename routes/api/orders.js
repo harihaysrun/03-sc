faed4e6c08fad76633aa7d909a09768c285907c2
harihaysrun <nursyahirah98@outlook.com>
2022-03-25 11:06:08 +0000

    add collection before fetch

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -6,7 +6,7 @@ const { OrderItem } = require('../../models');
 router.post('/', async function(req,res){
 
     let ordersArray = [];
-    let order = await OrderItem.where({
+    let order = await OrderItem.collection().where({
         'user_id': req.body.user_id
     }).fetch({
         withRelated:['shipping', 'user']
