2c5cae0bef92498b94d5facb31b1d666026b9177
harihaysrun <nursyahirah98@outlook.com>
2022-03-25 10:57:55 +0000

    add res send

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const { OrderItem } = require('../../models');
 
 router.post('/', async function(req,res){
-    return await OrderItem.where({
+    let order = await OrderItem.where({
         'user_id': req.body.user_id
     }).fetch({
         withRelated:['shipping', 'user']
     });
+
+    res.send(order)
 })
 
 module.exports = router;
