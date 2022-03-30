c7fe9efa63c3a9e7ef7cd8681c104fe6cc7e8c6e
harihaysrun <nursyahirah98@outlook.com>
2022-03-30 10:48:32 +0000

    add api route to get search values

--- routes/api/products.js
+++ routes/api/products.js
@@ -8,6 +8,20 @@ router.get('/', async function(req,res){
     res.send(await productDataLayer.getAllProducts())
 })
 
+router.get('/search', async function(req,res){
+
+    const allBrands = await productDataLayer.getAllBrands();
+    const allCountries = await productDataLayer.getAllCountries();
+    const allTypes = await productDataLayer.getAllTypes();
+
+    res.json({
+        'brands': allBrands,
+        'countries': allCountries,
+        'allTypes': allTypes
+    })
+    
+})
+
 router.get('/:product_id', async function(req,res){
 
     let productId = req.params.product_id;
